f8de87e1d6d0dc17561aff21f52f530c15d72dfe
r-sede <r-sede@orange.fr>
2017-06-25 20:15:12 +0200

    stringProd test et recusrive test

--- public/js/util.js
+++ public/js/util.js
@@ -16,33 +16,77 @@ StringProdSystem.prototype.addRules = function(predecessor,sucessor){
 	this.defineRules++;
 };
 
-StringProdSystem.prototype.convertAll = function(str,iter){
-	if(arguments.length === 0){
-		return this.convertAll(this.axiom,0);
-	}
 
-	if(iter>=this.desiredIter){
+
+StringProdSystem.prototype.convertAll = function(str,iter){
+	if(arguments.length === 0)
+		return this.convertAll(this.axiom,0);
+
+	if(iter>=this.desiredIter)
 		return str;
-	}
 
 	var iteration = iter;
 
 	if(this.defineRules === 1){
 		var tt = this.test1(str);
 		iteration++;
-		if(iteration===this.desiredIter){
+		if(iteration===this.desiredIter)
 			return tt;
-		}else{
+		else
 			return this.convertAll(tt,iteration);
-		}
+
 
 	}else{
 		var ttt=this.test(str);
 		iteration++;
-		if(iteration===this.desiredIter){
+		if(iteration===this.desiredIter)
 			return ttt;
-		}else{
+		else
 			return this.convertAll(ttt,iteration);
+	}
+
+};
+
+
+StringProdSystem.prototype.test = function(depart){
+	var strBuild ='';
+
+	for(var i = 0;i<depart.length;i++){
+		var cur = depart.charAt(i);
+		var find = false;
+		for(var ii=0;ii<this.prodRules.length;ii++){
+			if(cur == this.prodRules[ii].predecessor){
+				strBuild+=this.prodRules[ii].sucessor;
+				find = true;
+			}
 		}
+
+		if(!find)
+			strBuild+=cur;
+	}
+
+	return strBuild;
+};
+
+
+StringProdSystem.prototype.test1 = function(depart){
+	var strBuild = '';
+	for(var i = 0;i<depart.length;i++){
+		var cur = depart.charAt(i);
+		if(cur == this.prodRules[0].predecessor){
+			strBuild+=this.prodRules[0].sucessor;
+		}else{
+			strBuild+=cur;
+		}
+
+	}
+	return strBuild;
+};
+
+StringProdSystem.prototype.recursivTest = function(axiom, desIt){
+	if(desIt == 0){
+		return axiom;
+	}else{
+		return (this.prodRules.length === 1) ? this.recursivTest(this.test1(axiom), desIt-1):this.recursivTest(this.test(axiom),desIt-1);
 	}
 };
